c6059f37ee29bc17981c0369f2924147ee8234ef
markusneuy <markus.neuy@pixelpark.com>
2014-11-17 16:02:41 +0100

    Added a token for ppnet-proxy.

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 var config = require('./config.json');
 
@@ -15,10 +16,14 @@ proxy.on('error', function (error) {
     console.log(error);
 });
 
+
+app.use(cookieParser());
+
 /**
  * prevent the client from accessing /_utils or the like of the dbs
  */
 app.use(function(req, res, next) {
+    console.log(req.cookies);
     var currentdb = false;
     
     databases.forEach(function (db) {
@@ -28,15 +33,19 @@ app.use(function(req, res, next) {
     });
     
     if (currentdb) {
-        try {
-            proxy.web(req, res, { target: currentdb.remote });
-        } catch (error) {
-            console.log(error);
-            console.log("Proxying to " + currentdb.remote + " did not work.");
-            res.statusCode = 404;
+        if (req.cookies.token !== config.token) {
+            res.statusCode = 401;
             res.end();
+        } else {
+            try {
+                proxy.web(req, res, {target: currentdb.remote});
+            } catch (error) {
+                console.log(error);
+                console.log("Proxying to " + currentdb.remote + " did not work.");
+                res.statusCode = 404;
+                res.end();
+            }
         }
-        
     } else {
         next();
     }
